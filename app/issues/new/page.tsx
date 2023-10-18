e341caa47cf854192dace0cc0ab99c41e144e9bb
DyCodeBlock <iam.zandy88@gmail.com>
2023-10-18 10:39:14 +0800

    first commit

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { Button, Callout, Text, TextField, TextFieldInput } from "@radix-ui/themes"
+import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from 'zod';
+import ErrorMessage from "@/components/ErrorMessage";
+import Spinner from "@/components/Spinner";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+const NewIssuePage = () => {
+    const router = useRouter();
+    const {register, control, handleSubmit, formState: { errors }} = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
+    const [error, setError] = useState('');
+    const [isSubmitting, setSubmitting] = useState(false);
+
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setSubmitting(true);
+            // createIssue(data);
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+        } catch (error) {
+            setSubmitting(false);
+            setError('An unexpected error occurred.');
+        }
+    })
+
+  return (
+    <div className="max-w-xl">
+        {error && <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{error}</Callout.Text>    
+        </Callout.Root>}
+    <form 
+        className="space-y-3" 
+        onSubmit={onSubmit}
+        >
+            <TextField.Root>
+                <TextFieldInput placeholder="Title" {...register('title')} />
+            </TextField.Root>
+            <ErrorMessage>
+                {errors.title?.message}
+            </ErrorMessage>
+        <Controller 
+            name="description"
+            control={control}
+            render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+        />
+            <ErrorMessage>
+                {errors.description?.message}
+            </ErrorMessage>
+        <Button disabled={isSubmitting}>Submit New Issue{isSubmitting && <Spinner />}</Button>
+    </form>
+    </div>
+  )
+}
+
+export default NewIssuePage
---
+++ app/issues/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
+
+const IssuesPage = () => {
+  return (
+    <div><Button><Link href='/issues/new'>New Issue</Link></Button></div>
+  )
+}
+
+export default IssuesPage
